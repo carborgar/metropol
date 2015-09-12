ed9032b833555e926d65922d2e8592e4d96356ad
Juan Elias <juane.maireles@gmail.com>
2015-09-12 10:36:05 +0200

    Modelo de usuarios - Se ha creado en el modelo la clase usuario

--- metropol_abogados/models.py
+++ metropol_abogados/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Usuario(User):
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+        db_table = 'metropol_t_r_usu_usuario'
+
+    def __str__(self):
+        return self.last_name + ', ' + self.first_name
