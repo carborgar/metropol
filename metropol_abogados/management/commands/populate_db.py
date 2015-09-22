e8ef54ed28f22269f41888ad454e6d7f71012b56
Juan Elias <juane.maireles@gmail.com>
2015-09-22 17:00:42 +0200

    Permisos de la aplicacion Se ha creado los permisos que existe en la aplicacion

--- metropol_abogados/management/commands/populate_db.py
+++ metropol_abogados/management/commands/populate_db.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-
+from django.contrib.auth.models import Permission, User
+from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
 
 
@@ -10,17 +11,110 @@ class Command(BaseCommand):
     @staticmethod
     def insert_test_data():
         print('Populating with test data...')
+
+        user_carlos_borja = User(
+            username='carborgar',
+            first_name='Carlos',
+            last_name='Borja García - Baquero',
+            email='carlosborja93@gmail.com',
+        )
+        user_carlos_borja.set_password('metropol')
+        user_carlos_borja.save()
+        user_carlos_borja.user_permissions.add(Permission.objects.get(codename='management_metropol'))
+        user_carlos_borja.save()
+
+        user_juan_elias_maireles = User(
+            username='juamaiosu',
+            first_name='Juan Elias',
+            last_name='Maireles Osuna',
+            email='juane.maireles@gmail.com',
+        )
+        user_juan_elias_maireles.set_password('metropol')
+        user_juan_elias_maireles.save()
+        user_juan_elias_maireles.user_permissions.add(Permission.objects.get(codename='management_metropol'))
+        user_juan_elias_maireles.save()
+
         print('Populating with test data....OK\n')
 
     @staticmethod
     def insert_production_data():
         print('Populating with production data...')
+
+        user_alfonso_fernandez = User(
+            username='alffermac',
+            first_name='Alfonso',
+            last_name='Fernández Machuca',
+            email='alfonsoprofe10@gmail.com',
+        )
+        user_alfonso_fernandez.set_password('metropol')
+        user_alfonso_fernandez.save()
+        user_alfonso_fernandez.user_permissions.add(Permission.objects.get(codename='management_metropol'))
+        user_alfonso_fernandez.save()
+
+        user_santiago_machuca = User(
+            username='sanmacrod',
+            first_name='Santiago',
+            last_name='Machuca Rodríguez',
+            email='santiago.machuca@juntadeandalucia.es',
+        )
+        user_santiago_machuca.set_password('metropol')
+        user_santiago_machuca.save()
+        user_santiago_machuca.user_permissions.add(Permission.objects.get(codename='management_metropol'))
+        user_santiago_machuca.save()
+
+        user_antonia_carbonell = User(
+            username='antcarmor',
+            first_name='Antonia',
+            last_name='Carbonell Morilla',
+            email='tmorilla@gmail.com',
+        )
+        user_antonia_carbonell.set_password('metropol')
+        user_antonia_carbonell.save()
+        user_antonia_carbonell.user_permissions.add(Permission.objects.get(codename='management_metropol'))
+        user_antonia_carbonell.save()
+
+        user_ana_ruiz = User(
+            username='anaruigon',
+            first_name='Ana',
+            last_name='Ruiz González',
+            email='ruiz1989@hotmail.com',
+        )
+        user_ana_ruiz.set_password('metropol')
+        user_ana_ruiz.save()
+        user_ana_ruiz.user_permissions.add(Permission.objects.get(codename='management_metropol'))
+        user_ana_ruiz.save()
+
         print('Populating with production data....OK\n')
 
     @staticmethod
     def clean_tables():
+
         print('Dropping tables...')
+
+        User.objects.all().delete()
+
         print('Dropping tables....OK')
+
+    @staticmethod
+    def clean_permissions():
+
+        print('Dropping permissions...')
+
+        Permission.objects.all().delete()
+
+        print('Dropping permissions....OK')
+
+    @staticmethod
+    def insert_permissions():
+        print('Inserting permissions...')
+
+        Permission.objects.create(
+            codename='management_metropol',
+            name='Management Metropol',
+            content_type=ContentType.objects.get_for_model(User)
+        )
+
+        print('Inserting permissions....OK')
 
     def add_arguments(self, parser):
         parser.add_argument('--production', action='store_true', dest='production', default=False,
@@ -28,4 +122,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.clean_tables()
+        self.clean_permissions()
+        self.insert_permissions()
         self.insert_production_data() if options['production'] else self.insert_test_data()
--- metropol_abogados/models.py
+++ metropol_abogados/models.py
@@ -1,16 +1,1 @@
-from django.contrib.auth.models import User
 from django.db import models
-
-
-class Usuario(User):
-
-    class Meta:
-        ordering = ['last_name', 'first_name']
-        db_table = 'metropol_t_r_usu_usuario'
-        permissions = (
-            ('gestion_metropol', 'GESTION METROPOL'),
-            ('admin_metropol', 'ADMINISTRADOR METROPOL'),
-        )
-
-    def __str__(self):
-        return self.last_name + ', ' + self.first_name
